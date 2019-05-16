3a4a43a46ca81bb553160275b33f17a2327306ff
idler41 <idler41@163.com>
2019-05-16 13:59:16 +0800

    修改远程仓库2的api地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   // 代理服务器ip
-  // BASE_API: '"//10.0.0.72:8080/api/v1"'
-  BASE_API: '"//47.98.170.77/api/v1"'
+  BASE_API: '"//10.0.0.72:8080/api/v1"'
+  // BASE_API: '"//47.98.170.77/api/v1"'
 }
